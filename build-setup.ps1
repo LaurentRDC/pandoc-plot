cd4b08b1733d47f44523d8a8ec8fc6cfac7f4286
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-04-07 07:37:22 -0400

    [skip ci] setup fix

--- build-setup.ps1
+++ build-setup.ps1
@@ -6,7 +6,7 @@ stack clean --full
 Write-Host "Building executable"
 stack install pandoc-plot --local-bin-path ".\installer"
 
-$version = pandoc-plot --version
+$version = .\installer\pandoc-plot.exe --version
 Write-Host "Version: " $version
 
 Write-Host "Building setup using Inno Setup Compiler"
