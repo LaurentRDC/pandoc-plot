d1015a6a3520d9ad61a518f100d8ef5a7050a6e6
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-05-23 22:24:06 -0400

    Updated benchmarks

--- benchmark/bench.hs
+++ benchmark/bench.hs
@@ -1,9 +1,35 @@
+{-# LANGUAGE TemplateHaskell #-}
 
-import Criterion.Main (bench, defaultMain, nf)
+import Criterion.Main
+
 import Text.Pandoc.Definition
 import Text.Pandoc.Filter.Plot
 import Text.Pandoc.Filter.Plot.Internal
 
+import MatplotlibGallery (galleryItem1, galleryItem2)
+
 main :: IO ()
 main = 
-    defaultMain []
+    defaultMain [
+        envWithCleanup (return ()) (\_ -> cleanupEnv) $ \_ -> 
+            bgroup "main" [
+                  bench "single-threaded filter" $ nfIO (plotTransform singleThreadedConf benchDoc)
+                , bench "multi-threaded filter"  $ nfIO (plotTransform multiThreadedConf benchDoc)
+                ]
+    ]
+    where
+        singleThreadedConf = defaultConfiguration {allowParallel=False}
+        multiThreadedConf  = defaultConfiguration {allowParallel=True}
+
+cleanupEnv :: IO ()
+cleanupEnv = cleanOutputDirs defaultConfiguration benchDoc >> return ()
+
+
+codeBlock :: Script -> Block
+codeBlock = CodeBlock (mempty, [cls Matplotlib], mempty)
+
+
+benchDoc :: Pandoc
+benchDoc = Pandoc mempty [ codeBlock $(galleryItem1)
+                         , codeBlock $(galleryItem2)
+                         ]
