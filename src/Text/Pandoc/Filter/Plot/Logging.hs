ad305c7b4abab30a30701e7b8c4dfa149e78f076
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-06-10 15:31:46 -0400

    Added LoggingT transformer

--- src/Text/Pandoc/Filter/Plot/Logging.hs
+++ src/Text/Pandoc/Filter/Plot/Logging.hs
@@ -9,14 +9,13 @@ Maintainer  : laurent.decotret@outlook.com
 Stability   : internal
 Portability : portable
 
-Logging implementation. Inspired by the Hakyll implementation.
+Logging implementation. Inspired by Hakyll and monad-logger.
 -}
 
 module Text.Pandoc.Filter.Plot.Logging 
     ( Verbosity(..)
-    , Logger
     , LogSink(..)
-    , withLogger
+    , LoggingT(..)
     -- * Logging messages
     , debug
     , err
@@ -28,8 +27,9 @@ module Text.Pandoc.Filter.Plot.Logging
 import           Control.Concurrent      (forkIO)
 import           Control.Concurrent.Chan (Chan, newChan, readChan, writeChan)
 import           Control.Concurrent.MVar (MVar, newEmptyMVar, putMVar, takeMVar)
-import           Control.Monad           (forever)
+import           Control.Monad           (forever, mapM_)
 import           Control.Monad.Trans     (MonadIO, liftIO)
+import           Control.Monad.Writer    (WriterT, runWriterT, tell)
 
 import           Data.Char               (toLower)
 import           Data.String             (IsString(..))
@@ -39,6 +39,8 @@ import           Data.Yaml
 
 import           System.IO               (stderr)
 
+import           Prelude                 hiding (log)
+
 
 -- | Verbosity of the logger.
 data Verbosity = Silent   -- ^ Don't log anything.
@@ -47,6 +49,56 @@ data Verbosity = Silent   -- ^ Don't log anything.
                | Error    -- ^ Log information, warnings, and errors.
                | Debug    -- ^ Log all messages, including debug messages.
                deriving (Eq, Ord, Show)
+
+
+-- | Description of the possible ways to sink log messages.
+data LogSink = StdErr           -- ^ Standard error stream.
+             | LogFile FilePath -- ^ Appended to file.
+             deriving (Eq, Show)
+
+
+type LogMessage = (Verbosity, Text)
+
+type LoggingT m a = WriterT [LogMessage] m a
+
+
+runLoggingT :: MonadIO m => Verbosity -> LogSink -> LoggingT m a -> m a
+runLoggingT v StdErr       = runLoggingT' v $ mapM_ (TIO.hPutStrLn stderr . snd)
+runLoggingT v (LogFile fp) = runLoggingT' v $ mapM_ (TIO.appendFile fp . snd)
+
+
+runLoggingT' :: MonadIO m 
+            => Verbosity                -- ^ Minimum verbosity to keep
+            -> ([LogMessage] -> IO ())  -- ^ Log sink
+            -> LoggingT m a
+            -> m a
+runLoggingT' v f m = do
+    (r, t) <- runWriterT m
+    -- Messages with lower level than minimum are discarded
+    let t' = filter (\message -> fst message >= v) t
+    liftIO $ f t'
+    return r
+
+
+log :: Monad m => Verbosity -> Text -> LoggingT m ()
+log v t = tell [(v, t)]
+
+
+debug :: Monad m => Text -> LoggingT m ()
+debug t = log Debug $ "(DEBUG)   " <> t
+
+
+err :: Monad m => Text -> LoggingT m ()
+err t = log Error $ "(ERROR)   " <> t
+
+
+warning :: Monad m => Text -> LoggingT m ()
+warning t = log Warning $ "(WARNING) " <> t
+
+
+info :: Monad m => Text -> LoggingT m ()
+info t = log Info $ "          " <> t
+
 
 instance IsString Verbosity where
     fromString s
@@ -62,77 +114,3 @@ instance IsString Verbosity where
 instance FromJSON Verbosity where
     parseJSON (String t) = pure $ fromString . unpack $ t
     parseJSON _ = fail $ "Could not parse the logging verbosity."
-
-
--- | Description of the possible ways to sink log messages.
-data LogSink = StdErr           -- ^ Standard error stream.
-             | LogFile FilePath -- ^ Appended to file.
-             deriving (Eq, Show)
-
-
--- | Logger type.
---
--- The recommended usage of @Logger@ is to use the function @withLogger@. This ensures
--- that the logger is properly flushed after use.
-data Logger = Logger
-    { logVerbosity :: Verbosity         -- ^ Logger verbosity.
-    , logChannel   :: Chan (Maybe Text) -- ^ Logger channel.
-    , logOp        :: Text -> IO ()     -- ^ Logging operation (e.g. @putStrLn@, or @writeFile@).
-    , logSync      :: MVar ()           -- ^ Syncing variable.
-    }
-
-
--- | Create a logger and run an IO action. This function ensures that the
--- logger is properly flushed.
-withLogger :: Verbosity -> LogSink -> (Logger -> IO ()) -> IO ()
-withLogger v sink f = do
-    logger <- new v sink
-    f logger
-    flush logger
-
-
--- | Create a new logger.
-new :: Verbosity -> LogSink -> IO Logger
-new v sink = do
-    logger <- Logger <$> pure v <*> newChan <*> pure (logsink sink) <*> newEmptyMVar
-    _ <- forkIO $ loggerThread logger
-    return logger
-    where
-        logsink StdErr       = TIO.hPutStrLn stderr
-        logsink (LogFile fp) = TIO.appendFile fp
-
-        loggerThread Logger{..} = forever $ do
-            msg <- readChan logChannel
-            case msg of 
-                Nothing -> putMVar logSync ()
-                Just t  -> logOp t
-
-
--- | Flush the content of the logger.
-flush :: Logger -> IO ()
-flush Logger{..} = do
-    writeChan logChannel Nothing
-    () <- takeMVar logSync
-    return ()
-
-
-text :: MonadIO m => Logger -> Verbosity -> Text -> m ()
-text Logger{..} v t 
-    | (logVerbosity >= v) = liftIO $ writeChan logChannel (Just t)
-    | otherwise           = return ()
-
-
-debug :: MonadIO m => Logger -> Text -> m ()
-debug l t = text l Debug $ "(DEBUG)   " <> t
-
-
-err :: MonadIO m => Logger -> Text -> m ()
-err l t = text l Error $ "(ERROR)   " <> t
-
-
-warning :: MonadIO m => Logger -> Text -> m ()
-warning l t = text l Warning $ "(WARNING) " <> t
-
-
-info :: MonadIO m => Logger -> Text -> m ()
-info l t = text l Info $ "          " <> t
