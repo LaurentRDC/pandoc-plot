77bbf3329c0db486d75650c629bd01545ff9c3da
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-07-11 09:13:57 -0400

    Fix issue with finding the manual

--- executable/ManPage.hs
+++ executable/ManPage.hs
@@ -10,10 +10,12 @@ import           Data.String                (fromString)
 import           Data.Text                  (unpack)
 import qualified Data.Text.IO               as TIO
 
+import           System.FilePath            ((</>))
+
 import           Language.Haskell.TH.Syntax
 
 docFile :: FilePath
-docFile = "docs\\MANUAL.html"
+docFile = "docs" </> "MANUAL.html"
 
 readDocFile :: IO String
 readDocFile = TIO.readFile docFile >>= return . unpack 
