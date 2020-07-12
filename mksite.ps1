33122ec9531d252704029d9cc448d1014dd7b81e
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-07-12 10:24:22 -0400

    Updated theme

--- mksite.ps1
+++ mksite.ps1
@@ -1,6 +1,6 @@
 stack exec -- pandoc `
                 --standalone `
-                --css=docs/theme.css `
+                --css=theme.css `
                 --filter pandoc-plot `
                 -M "plot-configuration=docs/plot-config.yml" `
                 -i docs/index.md `
